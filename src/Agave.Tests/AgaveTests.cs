831992864ec46d8fc29afeb8f752b3b109508f6f
Bartul Bonacic <bartul.bonacic@live.com>
2024-03-22 15:07:21 +0100

    Add a reminder registry time aspect base

--- src/Agave.Tests/AgaveTests.cs
+++ src/Agave.Tests/AgaveTests.cs
@@ -1,5 +1,6 @@
 using Agave.Tests.TestExtensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Orleans.Runtime;
 using Orleans.Timers;
 
@@ -28,108 +29,55 @@ public partial class AgaveTests
     public async void GivenPlanted_When10SecondsPasses_ThenAgaveIsGerminated()
     {
         var state = new AgaveState();
-        IAgave agave = new Agave(new TestPersistentState<AgaveState>(state), new TestGrainContext(), new TestReminderRegistry(), _loggerFactory);
+        var reminderRegistry = new TestReminderRegistry();
+        IAgave agave = new Agave(new TestPersistentState<AgaveState>(state), new TestGrainContext(), reminderRegistry, _loggerFactory);
         await agave.Plant();
 
-        await Task.Delay(TimeSpan.FromSeconds(6));
+        reminderRegistry.AdvanceTime(TimeSpan.FromSeconds(6));
 
         Assert.Equal(AgaveBlossomState.Germinated, state.Current);
-    }
-
-}
-
-internal class TestGrainContext : IGrainContext
-{
-    public GrainReference GrainReference => throw new NotImplementedException();
-
-    public GrainId GrainId
-    {
-
-        get => GrainId.Create("Agave", Guid.NewGuid().ToString());
-
-    }
-
-    public object GrainInstance => throw new NotImplementedException();
-
-    public ActivationId ActivationId => throw new NotImplementedException();
-
-    public GrainAddress Address => throw new NotImplementedException();
-
-    public IServiceProvider ActivationServices => throw new NotImplementedException();
-
-    public IGrainLifecycle ObservableLifecycle => throw new NotImplementedException();
-
-    public IWorkItemScheduler Scheduler => throw new NotImplementedException();
-
-    public Task Deactivated => throw new NotImplementedException();
-
-    public void Activate(Dictionary<string, object> requestContext, CancellationToken? cancellationToken = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Deactivate(DeactivationReason deactivationReason, CancellationToken? cancellationToken = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool Equals(IGrainContext? other)
-    {
-        throw new NotImplementedException();
-    }
-
-    public TComponent GetComponent<TComponent>() where TComponent : class
-    {
-        throw new NotImplementedException();
-    }
-
-    public TTarget GetTarget<TTarget>() where TTarget : class
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Migrate(Dictionary<string, object> requestContext, CancellationToken? cancellationToken = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ReceiveMessage(object message)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Rehydrate(IRehydrationContext context)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void SetComponent<TComponent>(TComponent value) where TComponent : class
-    {
-        throw new NotImplementedException();
     }
 }
 
 internal class TestReminderRegistry : IReminderRegistry
 {
     private readonly Dictionary<GrainId, List<IGrainReminder>> _reminders = new();
+    private readonly IReminderRegistry _reminderRegistry;
+    private DateTime _currentTime = DateTime.UtcNow;
 
-    Task<IGrainReminder> IReminderRegistry.GetReminder(GrainId callingGrainId, string reminderName)
+    public void AdvanceTime(TimeSpan time)
     {
-        return Task.FromResult<IGrainReminder>(new TestGrainReminder(reminderName, TimeSpan.Zero, TimeSpan.Zero));
+        _currentTime += time;
     }
 
+    public TestReminderRegistry()
+    {
+        _reminderRegistry = this;
+    }
+    async Task<IGrainReminder> IReminderRegistry.GetReminder(GrainId callingGrainId, string reminderName)
+    {
+        var grainReminders = await _reminderRegistry.GetReminders(callingGrainId);
+        return grainReminders.Single(reminder => reminder.ReminderName == reminderName);
+    }
     Task<List<IGrainReminder>> IReminderRegistry.GetReminders(GrainId callingGrainId)
     {
-        return Task.FromResult(_reminders.GetValueOrDefault(callingGrainId) ?? new List<IGrainReminder>());
+        return Task.FromResult(_reminders.GetValueOrDefault(callingGrainId) ?? []);
     }
-
     Task<IGrainReminder> IReminderRegistry.RegisterOrUpdateReminder(GrainId callingGrainId, string reminderName, TimeSpan dueTime, TimeSpan period)
     {
-        return Task.FromResult<IGrainReminder>(new TestGrainReminder(reminderName, dueTime, period));
-    }
+        if (!_reminders.ContainsKey(callingGrainId))
+        {
+            _reminders[callingGrainId] = [];
+        }
+        _reminders[callingGrainId].RemoveAll(x => x.ReminderName == reminderName);
 
+        var reminder = new TestGrainReminder(reminderName, dueTime, period);
+        _reminders[callingGrainId].Add(reminder);
+        return Task.FromResult<IGrainReminder>(reminder);
+    }
     Task IReminderRegistry.UnregisterReminder(GrainId callingGrainId, IGrainReminder reminder)
     {
+        _reminders[callingGrainId]?.RemoveAll(x => x.ReminderName == reminder.ReminderName);
         return Task.CompletedTask;
     }
 }
---
+++ src/Agave.Tests/TestExtensions/TestGrainContext.cs
@@ -0,0 +1,75 @@
+using Orleans.Runtime;
+
+namespace Agave.Tests.TestExtensions;
+
+internal class TestGrainContext : IGrainContext
+{
+    public GrainReference GrainReference => throw new NotImplementedException();
+
+    public GrainId GrainId
+    {
+
+        get => GrainId.Create("Agave", Guid.NewGuid().ToString());
+
+    }
+
+    public object GrainInstance => throw new NotImplementedException();
+
+    public ActivationId ActivationId => throw new NotImplementedException();
+
+    public GrainAddress Address => throw new NotImplementedException();
+
+    public IServiceProvider ActivationServices => throw new NotImplementedException();
+
+    public IGrainLifecycle ObservableLifecycle => throw new NotImplementedException();
+
+    public IWorkItemScheduler Scheduler => throw new NotImplementedException();
+
+    public Task Deactivated => throw new NotImplementedException();
+
+    public void Activate(Dictionary<string, object> requestContext, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Deactivate(DeactivationReason deactivationReason, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Equals(IGrainContext? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TComponent GetComponent<TComponent>() where TComponent : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public TTarget GetTarget<TTarget>() where TTarget : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Migrate(Dictionary<string, object> requestContext, CancellationToken? cancellationToken = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReceiveMessage(object message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Rehydrate(IRehydrationContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetComponent<TComponent>(TComponent value) where TComponent : class
+    {
+        throw new NotImplementedException();
+    }
+}
+
