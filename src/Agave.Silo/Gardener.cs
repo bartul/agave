499c498e3981281051834ddd85bba4fc1463e9df
Bartul Bonacic <bartul.bonacic@live.com>
2024-05-13 22:06:36 +0200

    Add planting logic to gardner

--- src/Agave.Silo/Gardener.cs
+++ src/Agave.Silo/Gardener.cs
@@ -1,26 +1,32 @@
-using Agave.OrleansExtensions;
-
 namespace Agave;
 
 [GrainType("gardner")]
 [ImplicitChannelSubscription("event-bus")]
 public sealed class Gardner(
     IGrainContext grainContext,
+    IGrainFactory grainFactory,
     ILogger<Gardner> logger) : IGardner, IOnBroadcastChannelSubscribed
 {
     private readonly ILogger _logger = logger;
     private readonly IGrainContext _grainContext = grainContext;
+    private readonly IGrainFactory _grainFactory = grainFactory;
 
     public Task OnSubscribed(IBroadcastChannelSubscription streamSubscription)
     {
         return streamSubscription.Attach<SeedProduced>(e => {
             _logger.ReceiveSeedProducedEvent(_grainContext.GrainId);
+            _grainFactory.GetGrain<IAgave>(Guid.NewGuid()).Plant(new PlantSeedCommand(
+                TimeToGerminate: e.TimeToGerminate, 
+                SuccessRate: e.SuccessRate, 
+                DegenerationRate: e.DegenerationRate,
+                TimeToBlossom: e.TimeToBlossom,
+                NumberOfSeedsProducing: e.NumberOfSeedsProducing));
             return Task.CompletedTask;
         }, error => {
             _logger.ErrorReceivingSeedProducedEvent(error, _grainContext.GrainId);
             return Task.CompletedTask;
         });
-    }
+    }    
 }
 
 [Alias("Agave.IGardner")]
