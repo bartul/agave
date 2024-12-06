b7a9ddc185f8735fddc37d2f9e7388e5e54cc16a
Bartul Bonacic <bartul.bonacic@live.com>
2024-12-06 19:48:03 +0100

    Add storage to host

--- src/Agave.Silo.Host/Program.cs
+++ src/Agave.Silo.Host/Program.cs
@@ -1,10 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var storage = builder.AddAzureStorage("agave").RunAsEmulator();
+var storage = builder.AddAzureStorage("agave-storage").RunAsEmulator();
+var clusterTable = storage.AddTables("agave-clustering");
+var grainStorage = storage.AddBlobs("agave-grain-state");
 
 var orleans = builder.AddOrleans("default").WithDevelopmentClustering();
 
-builder.AddProject<Projects.Agave_Silo>("silo")
+builder.AddProject<Projects.Agave_Silo>("agave-silo")
     .WithReference(orleans)
     .WithReplicas(1);
 
